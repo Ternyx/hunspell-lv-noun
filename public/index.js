79d5689dfb377faabd101f5fe935ee0d33c0401b
ternyx <ternyx>
2020-10-17 12:31:02 +0300

    Basic frontend

---
+++ public/index.js
@@ -0,0 +1,9 @@
+const inputArea = document.getElementById('input-area');
+const submitButton = document.getElementById('submit-button');
+const outputArea = document.getElementById('output-area');
+
+submitButton.addEventListener('click', onSubmitClick);
+
+async function onSubmitClick() {
+    console.log('submit');
+}
